7afabd2523de6de1298976a17d9e07da11903ccd
Matthew Youngbar <matthewyoungbar@gmail.com>
2022-09-13 15:08:35 -0400

    App Skeleton

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+  data = request.get_json()
+
+  # We don't want to reply to ourselves!
+  if data['name'] != 'groupme-reply-bot':
+    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+    send_message(msg, data['id'])
+
+  return "ok", 200
+
+def send_message(msg, reply_id):
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+          'attachments':
+          {
+            "type": "reply",
+            "reply_id": reply_id,
+            "base_reply_id": reply_id
+          }
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
